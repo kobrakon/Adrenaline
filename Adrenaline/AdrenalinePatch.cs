4696d97a28442fc47bdccd9a8bee92ab47053ddd
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-12-22 16:24:33 -0500

    Update AdrenalinePatch.cs

--- Adrenaline/AdrenalinePatch.cs
+++ Adrenaline/AdrenalinePatch.cs
@@ -15,13 +15,18 @@ namespace Adrenaline
         {
             if (type == EDamageType.Bullet || type == EDamageType.Explosion || type == EDamageType.Sniper || type == EDamageType.Landmine || type == EDamageType.GrenadeFragment)
             {
-                if (__instance.ActiveHealthController.BodyPartEffects.Effects[0].Any(v => v.Key == "PainKiller"))
+                try
                 {
-                    (typeof(GClass1905).GetMethod("FindActiveEffect", BindingFlags.Instance | BindingFlags.Public).MakeGenericMethod(typeof(GClass1905).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[] { EBodyPart.Head }) as GClass1905.GClass1903).AddWorkTime(30f, true);
-                    return;
+                    if (__instance.ActiveHealthController.BodyPartEffects.Effects[0].Any(v => v.Key == "PainKiller"))
+                    {
+                        ActiveHealthControllerClass.GClass1914 pk = typeof(ActiveHealthControllerClass).GetMethod("FindActiveEffect", BindingFlags.Instance | BindingFlags.Public).MakeGenericMethod(typeof(ActiveHealthControllerClass).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[] { EBodyPart.Head }) as ActiveHealthControllerClass.GClass1914;
+                        if (pk.TimeLeft < 30) pk.AddWorkTime(30f, false);
+                        return;
+                    }
+                    MethodInfo method = typeof(ActiveHealthControllerClass).GetMethod("method_13", BindingFlags.Instance | BindingFlags.NonPublic);
+                    method.MakeGenericMethod(typeof(ActiveHealthControllerClass).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[]{ EBodyPart.Head, 0f, 30f, 5f, 1f, null });
                 }
-                MethodInfo method = typeof(GClass1905).GetMethod("method_13", BindingFlags.Instance | BindingFlags.NonPublic);
-                method.MakeGenericMethod(typeof(GClass1905).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[]{ EBodyPart.Head, 0f, 30f, 5f, 1f, null });;
+                catch (Exception) {} // sometimes it just kinda throws exceptions and idk why
             }
         }
     }
