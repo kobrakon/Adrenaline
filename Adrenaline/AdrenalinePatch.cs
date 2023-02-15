7eee69e1742dd18b9aa30303cad9a19414492a61
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2023-02-15 10:08:19 -0500

    update for 0.13

--- Adrenaline/AdrenalinePatch.cs
+++ Adrenaline/AdrenalinePatch.cs
@@ -1,8 +1,8 @@
 using EFT;
-using System.Reflection;
-using Aki.Reflection.Patching;
 using System;
 using System.Linq;
+using System.Reflection;
+using Aki.Reflection.Patching;
 
 namespace Adrenaline
 {
@@ -19,11 +19,11 @@ namespace Adrenaline
                 {
                     if (__instance.ActiveHealthController.BodyPartEffects.Effects[0].Any(v => v.Key == "PainKiller"))
                     {
-                        ActiveHealthControllerClass.GClass1914 pk = typeof(ActiveHealthControllerClass).GetMethod("FindActiveEffect", BindingFlags.Instance | BindingFlags.Public).MakeGenericMethod(typeof(ActiveHealthControllerClass).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[] { EBodyPart.Head }) as ActiveHealthControllerClass.GClass1914;
+                        ActiveHealthControllerClass.GClass2091 pk = typeof(ActiveHealthControllerClass).GetMethod("FindActiveEffect", BindingFlags.Instance | BindingFlags.Public).MakeGenericMethod(typeof(ActiveHealthControllerClass).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[] { EBodyPart.Head }) as ActiveHealthControllerClass.GClass2091;
                         if (pk.TimeLeft < 30) pk.AddWorkTime(30f, false);
                         return;
                     }
-                    MethodInfo method = typeof(ActiveHealthControllerClass).GetMethod("method_13", BindingFlags.Instance | BindingFlags.NonPublic);
+                    MethodInfo method = typeof(ActiveHealthControllerClass).GetMethod("method_15", BindingFlags.Instance | BindingFlags.NonPublic);
                     method.MakeGenericMethod(typeof(ActiveHealthControllerClass).GetNestedType("PainKiller", BindingFlags.Instance | BindingFlags.NonPublic)).Invoke(__instance.ActiveHealthController, new object[]{ EBodyPart.Head, 0f, 30f, 5f, 1f, null });
                 }
                 catch (Exception) {} // sometimes it just kinda throws exceptions and idk why
