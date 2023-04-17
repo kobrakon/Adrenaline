4327b658881bbb08fd793d1200db2240772fda15
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2023-04-16 22:34:42 -0400

    Update Plugin.cs

--- Adrenaline/Plugin.cs
+++ Adrenaline/Plugin.cs
@@ -5,9 +5,6 @@ namespace Adrenaline
     [BepInPlugin("com.kobrakon.adrenaline", "Adrenaline", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-        void Awake()
-        {
-            new AdrenalinePatch().Enable();
-        }
+        void Awake() => new AdrenalinePatch().Enable();
     }
 }
